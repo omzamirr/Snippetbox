f79c99c760ae96896824f1abc67c25fc3f487085
omzamirr <zamiromarnajib@gmail.com>
2025-09-06 20:23:07 +0600

    Implemented RESTFUL Routes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,12 +10,7 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 
-	
 
 	s, err := app.snippets.Latest()
 	if err != nil {
@@ -49,7 +44,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -92,12 +87,13 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	
 }
 
+func (app * application) createSnippetForm(w http.ResponseWriter, r * http.Request) {
+	app.render(w, r, "create.page.tmpl", nil)
+
+}
+
+
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
 
 	title := "O snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi"
@@ -109,7 +105,9 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
+
+
 
 
