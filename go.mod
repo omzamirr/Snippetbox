514ba858fe8fa9ca3413005778a0d939245d660e
omzamirr <zamiromarnajib@gmail.com>
2025-09-07 20:59:06 +0600

    Added: User Login Functionality

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/omzamirr/snippetbox
 go 1.24.2
 
 require (
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
+	github.com/go-sql-driver/mysql v1.9.3
+	github.com/golangcollege/sessions v1.2.0
+	github.com/justinas/alice v1.2.0
+	golang.org/x/crypto v0.0.0-20200317142112-1b76d66859c6
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
-	github.com/go-sql-driver/mysql v1.9.3 // indirect
-	github.com/golangcollege/sessions v1.2.0 // indirect
-	github.com/justinas/alice v1.2.0 // indirect
-	golang.org/x/crypto v0.0.0-20200317142112-1b76d66859c6 // indirect
 	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
 )
--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -3,6 +3,7 @@ package mysql
 
 import (
 	"database/sql"
+	
 	"strings"
 	"github.com/omzamirr/snippetbox/pkg/models"
 
@@ -40,7 +41,28 @@ func (m *UserModel) Insert(name, email, password string) error {
 
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	var id int 
+
+	var hashedPassword []byte
+
+	row := m.DB.QueryRow("SELECT id, hashed_password FROM users WHERE email = ?", email)
+
+	err := row.Scan(&id, &hashedPassword)
+	if err == sql.ErrNoRows {
+		return 0, models.ErrInvalidCredentials
+	} else if err != nil {
+		return 0, err
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err == bcrypt.ErrMismatchedHashAndPassword {
+		return 0, models.ErrInvalidCredentials
+	} else if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+
 }
 
 
