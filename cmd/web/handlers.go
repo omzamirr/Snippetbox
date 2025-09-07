15120c1f79753b41a843c4ad1750f8d6fc40cb32
omzamirr <zamiromarnajib@gmail.com>
2025-09-07 15:49:11 +0600

    added snippet creation confirmation for users

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -39,11 +39,12 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	flash := app.session.PopString(r, "flash")
+
 	app.render(w, r, "show.page.tmpl", &templateData{
+		Flash:   flash,
 		Snippet: s,
 	})
-
-
 }
 
 func (app * application) createSnippetForm(w http.ResponseWriter, r * http.Request) {
@@ -77,6 +78,8 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
+
+	app.session.Put(r, "flash", "Snippet successfully created!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,13 +8,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 	"github.com/omzamirr/snippetbox/pkg/models/mysql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/golangcollege/sessions"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog	 *log.Logger
+	session  *sessions.Session
 	snippets *mysql.SnippetModel
 	templateCache map[string]*template.Template
 }
@@ -23,6 +26,8 @@ func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL database connection string")
+
+	secret := flag.String("secret", "s6Ndh+pPbnzHbS*+9Pk8qGWhTzbpa@ge", "Secret key for authentication")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -40,9 +45,13 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
+	session := sessions.New([]byte(*secret))
+	session.Lifetime = 12 * time.Hour
+
 	app := &application{
 		errorLog:	errorLog,
 		infoLog:	infoLog,
+		session:	session,
 		snippets:	&mysql.SnippetModel{DB: db},
 		templateCache: templateCache,
 	}
